9e9be0733337dd2f5376a7b8c0a8900c445bce24
Absolom Khanye <khanyeinc@gmail.com>
2025-07-13 04:27:01 +0200

    Add Scalar for API Documentation
    Adds the nuget package and configure the scalar UI setup.

--- SampleApi/Program.cs
+++ SampleApi/Program.cs
@@ -1,15 +1,22 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.Title = "The Sample API";
+        options.Theme = ScalarTheme.Saturn;
+        options.Layout = ScalarLayout.Modern;
+        options.HideClientButton = true;    
+    });
 }
 
 app.UseHttpsRedirection();
